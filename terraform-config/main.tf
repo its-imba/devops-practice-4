63199bcc1b08fc3d46f4987170ecb039bf8ce18c
its-imba <its.sam.imba@gmail.com>
2023-04-19 17:06:06 +0100

    added outbound rule

--- terraform-config/main.tf
+++ terraform-config/main.tf
@@ -32,6 +32,15 @@ resource "aws_security_group" "standard_access" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+# Outbound rule
+egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+
 }
 
 # Create a VM resource
