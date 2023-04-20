918a6a7bd29070407b0f2f44c920edef1ef766d8
its-imba <its.sam.imba@gmail.com>
2023-04-20 10:31:40 +0100

    added software installer script

---
+++ software-installer.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+sudo apt update
+
+#Install Java for Jenkins dependancy
+
+sudo apt install -y openjdk-11-jdk
+
+sudo apt update
+
+# Install misc dependancies
+
+sudo apt-get -y install \
+    ca-certificates \
+    curl \
+    gnupg2
+
+sudo apt update
+
+# Install Jenkins
+
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+sudo apt update
+
+sudo apt-get -y install jenkins
+
+# Install Docker
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
